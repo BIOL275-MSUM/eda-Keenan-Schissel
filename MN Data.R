8b4e9b03c16c18d36c4e9963813ed4395c9d3e48
Keenan Schissel <keenan.schissel@gmail.com>
2021-04-15 10:55:44 -0500

    Commit this i suppose

--- MN Data.R
+++ MN Data.R
@@ -7,4 +7,16 @@ library(readxl)
 
 #Read Data
 
-download_data <- read_excel("Data/2020 County Health Rankings Minnesota Data - v1_0 (1).xlsx")
+download_data <- read_excel("Data/2020 County Health Rankings Minnesota Data - v1_0 (1).xlsx",
+    sheet= "Ranked Measure Data", skip=1)
+
+download_data
+glimpse(download_data)
+transmute(
+  download_data,
+  FIPS,
+  State,
+  County,
+  Deaths,
+  
+)
