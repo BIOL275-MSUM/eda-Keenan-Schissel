a7a4dd33db45b772fb01cadeb4986b9cdfb0e7ce
Keenan Schissel <keenan.schissel@gmail.com>
2021-04-20 10:46:16 -0500

    Pushing before SAC

--- MN Data.R
+++ MN Data.R
@@ -2,21 +2,57 @@
 
 library(tidyverse)
 
-
 library(readxl)
 
+install.packages("corrplot")
+
+library(corrplot)
+library(ggplot2)
 #Read Data
 
 download_data <- read_excel("Data/2020 County Health Rankings Minnesota Data - v1_0 (1).xlsx",
     sheet= "Ranked Measure Data", skip=1)
 
 download_data
+
 glimpse(download_data)
-transmute(
-  download_data,
+
+
+Ranked_meas_data <- transmute(
+download_data,
   FIPS,
   State,
   County,
   Deaths,
-  
+  violent_crime = 'violent Crime Rate' ,
+ FP_health = 'Fair or poor Health' ,
+  Unemployment = '% Unemployed' ,
+  Income = 'Income Ratio' ,
 )
+
+Ranked_meas_data
+
+# plot------------------------------------------------------------------------
+
+
+Ranked_meas_data %>%
+  filter(!is.na(Count)) %>%  
+  select(-FIPS,-State,-County,-Deaths) %>%
+  cor(use = "na.or.complete") %>% 
+  corrplot(
+    method = "color",
+    type = "upper",
+    order = "hclust",
+    tl.col ="black" ,
+    tl.srt = 45 ,
+    addCoef.col = "black"
+  )  
+
+ggplot(Ranked_meas_data, aes(x = violent_crime, y= FP_health )) +
+  geom_point() +
+  geom_smooth(method = lm, se = TRUE)
+
+ggplot( Ranked_meas_data, aes(x = Unemployment, y = Income)) +
+  geom_point() +
+  geom_smooth(method = lm, se = TRUE)
+
